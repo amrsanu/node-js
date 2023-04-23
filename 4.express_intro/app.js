0f0357deab00574d0778862ad2e8801efcdf8614
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-04-23 10:10:34 +0530

    Adding basic start to nodejs

--- 4.express_intro/app.js
+++ 4.express_intro/app.js
@@ -7,8 +7,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 // Use defined packages
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 // Parses the html body(forms here) and calls next(). 
@@ -16,13 +19,17 @@ app.use(bodyParser.urlencoded({
     extended: false,
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 //  Using the routes defined in other packages.
 // Can have filter for routes using the pre route.
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+    })
 });
 
 app.listen(3000);
--- 4.express_intro/routes/admin.js
+++ 4.express_intro/routes/admin.js
@@ -1,23 +1,29 @@
-// For ADMIN: To handle the creation of products
-
-const express = require('express');
 const path = require('path');
 
-const rootDir = require('../utils/path');
+const express = require('express');
 
+const rootDir = require('../util/path');
 
-// Router: Mini express to handle the routes. 
 const router = express.Router();
 
-// GET/POST... makes sure to match the exact route.
+const products = [];
+
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-// Only for POST: /admin/add-product
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
