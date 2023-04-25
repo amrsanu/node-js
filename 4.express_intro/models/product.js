c428340dc77622209bf5935e773eeec7c3cac476
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-04-25 22:49:25 +0530

    used SQL for few features - will use Sequelize for complete voew

--- 4.express_intro/models/product.js
+++ 4.express_intro/models/product.js
@@ -1,25 +1,8 @@
-const fs = require('fs');
-const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-
+const db = require('../util/database');
 const Cart = require('./cart');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
+// Model for storing Products with the propoerties.
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
     this.id = id;
@@ -30,45 +13,18 @@ module.exports = class Product {
   }
 
   save() {
-
-    getProductsFromFile(products => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-        });
-      } else {
-        this.id = uuidv4().toString();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
+  return db.execute(`INSERT INTO products (title, imageUrl, description, price) 
+    VALUES (?, ?, ?, ?)`, [this.title, this.imageUrl, this.description, this.price])
   }
 
   static deleteByID(id) {
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (!err) {
-          Cart.deleteProduct(id, product.price);
-        }
-      });
-    });
   }
 
   static fetchAll(cb) {
-    getProductsFromFile(cb);
+    return db.execute('SELECT * FROM products');
   }
 
   static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id);
-      cb(product);
-    });
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
   }
 };
